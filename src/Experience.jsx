b445195f54d81d52f2e809297badf4897c30943e
Josiah Webb <jj.webb520@gmail.com>
2023-01-30 15:10:14 -0700

    adds postprocessing to packages, adds effectComposer and vingette to scene

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,9 +1,21 @@
 import { OrbitControls } from '@react-three/drei'
+import { EffectComposer, Vignette } from '@react-three/postprocessing'
 import { Perf } from 'r3f-perf'
+import { BlendFunction } from 'postprocessing'
 
 export default function Experience()
 {
     return <>
+        
+        {/* this is typically housed in separate component, often called 'Effects' */}
+        <EffectComposer
+            multisampling={ 8 } // aliasing controls, default is 8
+        >
+            <Vignette
+                offset={ .3 }
+                darkness={ .9 }
+            />
+        </EffectComposer>
 
         <Perf position="top-left" />
 
