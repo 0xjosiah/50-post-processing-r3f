0dd8037333ff2577b1ecf6fb14f7bcf0a6090be9
Josiah Webb <jj.webb520@gmail.com>
2023-01-30 18:20:03 -0700

    retrieves uniforms and uses them in frag shader

--- src/DrunkEffect.js
+++ src/DrunkEffect.js
@@ -1,8 +1,12 @@
 import { Effect } from 'postprocessing'
+import { Uniform } from 'three'
 
 const fragmentShader = /* glsl */ `
+    uniform float frequency;
+    uniform float amplitude;
+
     void mainUv(inout vec2 uv) {
-        uv.y += sin(uv.x * 10.0) * 0.1;
+        uv.y += sin(uv.x * frequency) * amplitude;
     }
 
     void mainImage(const in vec4 inputColor, const in vec2 uv, out vec4 outputColor) {
@@ -20,8 +24,9 @@ export default class DrunkEffect extends Effect {
             fragmentShader,
             {
                 uniforms: new Map([
+                    // two ways of adding uniforms
                     [ 'frequency', { value: frequency }],
-                    [ 'amplitude', { value: amplitude }]
+                    [ 'amplitude', new Uniform(amplitude)]
                 ])
             }
         )
