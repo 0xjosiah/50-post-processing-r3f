201ae9c245d604ae7302ace0ee32e3c6402aa84f
Josiah Webb <jj.webb520@gmail.com>
2023-01-30 17:47:55 -0700

    writes fragmentShader for custom drunk effect class

--- src/DrunkEffect.js
+++ src/DrunkEffect.js
@@ -2,7 +2,7 @@ import { Effect } from 'postprocessing'
 
 const fragmentShader = /* glsl */ `
     void mainImage(const in vec4 inputColor, const in vec2 uv, out vec4 outputColor) {
-        
+        outputColor = vec4(uv, 1.0, 1.0);
     }
 `
 
