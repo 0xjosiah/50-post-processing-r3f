b99ba1bdb14b571f47fcab13aac0f988ee6affc3
Josiah Webb <jj.webb520@gmail.com>
2023-01-30 18:16:40 -0700

    adds mainUv fn to frag shader

--- src/DrunkEffect.js
+++ src/DrunkEffect.js
@@ -1,6 +1,10 @@
 import { Effect } from 'postprocessing'
 
 const fragmentShader = /* glsl */ `
+    void mainUv(inout vec2 uv) {
+        uv.y += sin(uv.x * 10.0) * 0.1;
+    }
+
     void mainImage(const in vec4 inputColor, const in vec2 uv, out vec4 outputColor) {
         vec4 color = inputColor;
         color.rgb *= vec3(0.8, 1.0, 0.5);
@@ -10,12 +14,15 @@ const fragmentShader = /* glsl */ `
 
 export default class DrunkEffect extends Effect {
 
-    constructor(props) {
+    constructor({ frequency, amplitude }) {
         super(
             'DrunkEffect',
             fragmentShader,
             {
-
+                uniforms: new Map([
+                    [ 'frequency', { value: frequency }],
+                    [ 'amplitude', { value: amplitude }]
+                ])
             }
         )
     }
