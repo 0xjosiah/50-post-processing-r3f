0f0859e8c86d0a6ee050f8b9db09a142d0b2069c
Josiah Webb <jj.webb520@gmail.com>
2023-01-30 17:43:33 -0700

    creates DrunkEffect class for custom effect

---
+++ src/DrunkEffect.js
@@ -0,0 +1,14 @@
+import { Effect } from 'postprocessing'
+
+const fragmentShader = /* glsl */ `
+    void mainImage(const in vec4 inputColor, const in vec2 uv, out vec4 outputColor) {
+        
+    }
+`
+
+export default class DrunkEffect extends Effect {
+
+    constructor() {
+        super()
+    }
+}
